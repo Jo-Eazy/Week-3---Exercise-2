4e3019ef16e3a28d571c763ea7883bcf8344c912
Jo-Eazy <josedhlamini9@gmail.com>
2025-10-29 09:07:28 +0200

    Add files via upload

---
+++ operations-types.js
@@ -0,0 +1,19 @@
+let number1 = 6;
+
+let number2 = 7;
+
+let number1String = number1.toString();
+
+let sum = number1 + number2;
+
+let product = number1 * number2;
+
+console.log("Number 1 is:", number1);
+
+console.log("Number 2 is:", number2);
+
+console.log("Number 1...but as a string:", number1String);
+
+console.log("The sum addition of Number 1 and Number 2 is:", sum);
+
+console.log("Therefore, the product of Number 1 and Number 2 multiplied together is:", product);
